b3823b44927fb136eb46eb02ea26e95ce4dd88a9
exitdump <sheikhforid1026@gmail.com>
2020-09-27 21:30:30 +0600

    post added

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express = require('express');
-
+const  cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+const users = ['shaba', 'nilima', 'udhas', 'mahbub', 'faisal', 'niloy'];
 
-app.get('/', (req, res) => { res.send('Thank You! Node') })
 
-app.listen(3000, () => console.log('Listening to port 3000'));
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(req.query);
+    const name = users[id];
+    res.send({id, name});
+})
+
+//post
+app.post('/addUser', (req, res) => {
+    //save to database
+    const user = req.body;
+    user.id = 55;
+    res.send(user);
+})
+app.listen(4000, () => console.log('Listening to port 4000'));
